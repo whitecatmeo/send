a4d6b7364f15710d021868ec3028ec6c2bb8ab84
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2023-02-26 11:32:28 +0800

    Add files via upload

---
+++ 小羊羊生日登入頁面.js
@@ -0,0 +1,11 @@
+function password(pwd)
+        {
+        if(pwd!="寶貝小羊羊生日快樂5201314")
+        {
+        location.href("http://whitetiger.eu.org/%E9%9B%BB%E8%85%A6%E7%89%88/%E5%AF%86%E7%A2%BC%E8%BC%B8%E5%85%A5%E9%8C%AF%E8%AA%A4.html")
+        }
+        else
+        location.href("https://loveyou1314and15yearshappybirthday.on.drv.tw/catcatmeow/%E7%94%9F%E6%97%A5%E5%BD%B1%E7%89%87.html")
+        {
+        }
+}
